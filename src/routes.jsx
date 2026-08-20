9288019939dc51f926c77a17cc41ad8d0e4354e6
adriana <adriana.santos@sudoesteinformatica.com.br>
2026-08-20 17:39:02 -0300

    .

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,32 +1,25 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import App from "./App";
-import AuthRoute from "./pages/auth/authRoutes";
-import UserRoute from "./pages/user/userRoute";
-import EquipamentoRoute from "./pages/equipamentos/equipamentoRoute";
-import FornecedorRoute from "./pages/fornecedor/FornecedorRoute";
-import ColaboradorRoute from "./pages/colaborador/colaboradorRoute";
+import AuthRoute from "./app/routes/authRoutes";
+import UserRoute from "./features/user/userRoute";
+// import EquipamentoRoute from "./pages/equipamentos/equipamentoRoute";
+// import FornecedorRoute from "./pages/fornecedor/FornecedorRoute";
+// import ColaboradorRoute from "./pages/colaborador/colaboradorRoute";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Rotas de Autenticação (sem o menu lateral do App) */}
+        <Route path="/" element={<Navigate to="/auth" replace />} />
         <Route path="/auth/*" element={<AuthRoute />} />
-
-        {/* Layout Principal (Com SideMenu e Outlet) */}
         <Route path="/" element={<App />}>
-          {/* Ao acessar "/", redireciona para "/users" */}
           <Route index element={<Navigate to="/users" replace />} />
-
           <Route path="users/*" element={<UserRoute />} />
-
-          <Route path="equipamentos/*" element={<EquipamentoRoute />} />
-
-          <Route path="fornecedores/*" element={<FornecedorRoute/>}/>
-
-          <Route path="colaboradores/*" element={<ColaboradorRoute/>}/>
-          
+          {/* <Route path="equipamentos/*" element={<EquipamentoRoute />} />
+          <Route path="fornecedores/*" element={<FornecedorRoute />} />
+          <Route path="colaboradores/*" element={<ColaboradorRoute />} /> */}
+          <Route path="*" element={<Navigate to="/auth" replace />} />
         </Route>
       </Routes>
     </BrowserRouter>
