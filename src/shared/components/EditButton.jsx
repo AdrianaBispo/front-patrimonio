626533a46985877a8876d3297c0e363e9322eb84
adrianaBispo283 <adriana.bispo283@gmail.com>
2026-08-30 19:42:59 -0300

    feat: adicionado onClick

--- src/shared/components/EditButton.jsx
+++ src/shared/components/EditButton.jsx
@@ -1,6 +1,6 @@
-export default function EditButton() {
+export default function EditButton({onClick}) {
   return (
-    <button href="#" class="text-text-primary hover:text-text-primary">
+    <button onClick={onClick} class="text-text-primary hover:text-text-primary">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
