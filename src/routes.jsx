fbb52bf2703d5f971b285a26b67df8c3f8fb78d6
AdrianaBispo <adriana.bispo283@gmail.com>
2026-09-11 10:43:08 -0300

    ajustado o import

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App";
 import AuthRoute from "./app/routes/authRoutes";
 import UserRoute from "./app/routes/userRoute";
-// import EquipamentoRoute from "./pages/equipamentos/equipamentoRoute";
+import EquipamentoRoute from "./app/routes/equipamentoRoute";
 // import FornecedorRoute from "./pages/fornecedor/FornecedorRoute";
 import ColaboradorRoute from "./app/routes/colaboradorRoute";
 
@@ -16,8 +16,8 @@ export default function Router() {
         <Route path="/" element={<App />}>
           <Route index element={<Navigate to="/users" replace />} />
           <Route path="users/*" element={<UserRoute />} />
-          {/* <Route path="equipamentos/*" element={<EquipamentoRoute />} />
-          <Route path="fornecedores/*" element={<FornecedorRoute />} />*/}
+          <Route path="equipamentos/*" element={<EquipamentoRoute />} />
+           {/* <Route path="fornecedores/*" element={<FornecedorRoute />} /> */}
           <Route path="colaboradores/*" element={<ColaboradorRoute />} /> 
           <Route path="*" element={<Navigate to="/auth" replace />} />
         </Route>
