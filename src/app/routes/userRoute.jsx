c6d28d77a79f38a3775cea05b5635d5ef267c942
AdrianaBispo <adriana.bispo283@gmail.com>
2026-09-11 10:30:10 -0300

    movido para a pasta routes

--- src/app/routes/userRoute.jsx
+++ src/app/routes/userRoute.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
-import UserListView from "./pages/userListPage";
-import CadastrarUserPage from "./pages/CadastrarUsuarioPage";
-import EditarUsuarioPage from "./pages/EditarUsuarioPage";
+import UserListView from "../../features/user/pages/userListPage";
+import CadastrarUserPage from "../../features/user/pages/CadastrarUsuarioPage";
+import EditarUsuarioPage from "../../features/user/pages/EditarUsuarioPage";
 
 export default function UserRoute() {
   return (
