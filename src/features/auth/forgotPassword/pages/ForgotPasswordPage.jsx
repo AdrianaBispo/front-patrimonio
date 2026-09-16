7f083d19f76710ab759aa3382c7f93823b4d7805
AdrianaBispo <adriana.bispo283@gmail.com>
2026-09-16 17:32:20 -0300

    feat: adicionado o useForgotPassword

--- src/features/auth/forgotPassword/pages/ForgotPasswordPage.jsx
+++ src/features/auth/forgotPassword/pages/ForgotPasswordPage.jsx
@@ -1,68 +1,94 @@
 import { Link } from "react-router-dom";
+import { useForgotPassword } from "../hooks/useForgotError";
 
 function ForgotPasswordPage() {
+  const { email, setEmail, errors, submitLoading, changePassword } = useForgotPassword();
+
+async function handleSubmit(e) {
+    e.preventDefault();
+    await changePassword({ email });
+  }
+
   return (
     <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2 bg-bg-primary">
-      <div class="flex min-h-full flex-col justify-center px-6 py-12">
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-          <div class="rounded-md bg-secondary p-2 ring-1 ring-white/10 text-white max-w-10">
+      <div className="flex min-h-full flex-col justify-center px-6 py-12">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="rounded-md bg-secondary p-2 ring-1 ring-white/10 text-white max-w-10">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
-              class="size-6"
+              className="size-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
               />
             </svg>
           </div>
-          <h2 class="mt-10 text-2xl text-left font-bold tracking-tight text-bold text-text-primary">
-            Recuperar Senha{" "}
+          <h2 className="mt-10 text-2xl text-left font-bold tracking-tight text-bold text-text-primary">
+            Recuperar Senha
           </h2>
         </div>
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" class="space-y-6">
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div>
-              <label for="user">E-mail</label>
-              <div class="mt-2">
+              <label htmlFor="user">E-mail</label>
+              <div className="mt-2">
                 <input
                   id="user"
                   type="email"
-                  max={20}
+                  maxLength={256}
                   className="input-utilities"
                   placeholder="name@example.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   required
-                ></input>
+                />
               </div>
+              {errors?.email && (
+                <span className="text-sm text-red-500 mt-1 block">
+                  {errors.email}
+                </span>
+              )}
             </div>
 
             <div>
-              <button type="submit" className="btn-primary w-full">
-                Recuperar Acesso
+              <button
+                type="submit"
+                className="btn-primary w-full"
+                disabled={submitLoading}
+              >
+                {submitLoading ? (
+                  <div className="flex flex-col items-center gap-4">
+                    <div className="h-6 w-6 animate-spin rounded-full border-2 border-bg-primary border-t-transparent" />
+                  </div>
+                ) : (
+                  "Recuperar Acesso"
+                )}
               </button>
             </div>
 
-            <div class="text-center">
+            <div className="text-center">
               <Link
                 to="/auth/login"
-                class="inline-flex items-center gap-2 text-sm text-muted-foreground hover:text-foreground transition-colors"
+                className="inline-flex items-center gap-2 text-sm text-muted-foreground hover:text-foreground transition-colors"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
-                  class="size-4"
+                  className="size-4"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
                   />
                 </svg>
@@ -73,7 +99,7 @@ function ForgotPasswordPage() {
         </div>
       </div>
 
-      <div class="bg-[url(https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png)] hidden lg:block"></div>
+      <div className="bg-[url(https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png)] hidden lg:block" />
     </div>
   );
 }
